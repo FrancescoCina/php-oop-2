448fc85e5734a34e53450e2a3a66b84145e5721f
FrancescoCina <francesco.cina04@gmail.com>
2021-10-06 16:23:00 +0200

    Fatte due classi User e UserPremium

---
+++ models/User.php
@@ -0,0 +1,75 @@
+<?php
+
+class User
+{
+    protected $name;
+    protected $email;
+    protected $age;
+    protected $discount;
+
+
+
+    // CONSTRUCTOR
+    public function __construct($_name, $_email, $_age)
+    {
+        $this->name = $_name;
+        $this->email = $_email;
+        $this->age = $_age;
+    }
+
+
+
+    // GETTER
+    protected function getName()
+    {
+        return $this->name;
+    }
+    protected function getDiscount()
+    {
+        return $this->discount;
+    }
+    protected function getEmail()
+    {
+        return $this->email;
+    }
+
+    protected function getAge()
+    {
+        return $this->age;
+    }
+
+
+
+    // SETTER
+
+    public function setEmail()
+    {
+        return $this->email;
+    }
+
+
+
+
+    public function setDiscount()
+    {
+        if ($this->age < 25) {
+            return $discount = 25;
+        } else {
+            return $discount = 0;
+        }
+    }
+
+
+    // METHODS
+    public function echoName()
+    {
+        echo $this->getName();
+    }
+}
+
+$user1 = new User('Francesco', 'fra@gmail', '21');
+
+echo $user1->setEmail();
+echo '<br/>';
+echo $user1->setDiscount();
+echo '<br/>';
---
+++ models/UserPremium.php
@@ -0,0 +1,32 @@
+<?php
+
+
+require_once __DIR__ . '/User.php';
+
+class UserPremium extends User
+{
+
+    private $seniority;
+
+    // CONSTRUCTOR
+
+    public function __construct($_name, $_email, $_age, $_seniority)
+    {
+        parent::__construct($_name, $_email, $_age);
+        $this->seniority = $_seniority;
+    }
+
+    // SETTER
+    public function setDiscount()
+    {
+        if ($this->seniority > 10) {
+            return $discount = 58;
+        } else {
+            return $discount = 10;
+        }
+    }
+}
+
+
+$user2 = new UserPremium('Mario', 'Rossi', '18', '15');
+echo 'lo sconto per il secondo utente è: ' . $user2->setDiscount();
